c69d2d77bc9a5897caf59151dddaec7cead9fc77
Rory <rory@localhost.localdomain>
2019-07-28 16:27:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Article from './Article';
+
+class SearchBar extends React.Component {
+  render() {
+    return <input className="search-bar" type="text" onKeyUp={evt => this.props.onInput(evt.target.value)}></input>
+  }
+}
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      search_query: ""
+    };
+  }
+
+  componentWillMount() {
+    this.getArticles();
+  }
+
+  getArticles() {
+    let articles = [];
+    const url = this.state.search_query === "" ? "http://localhost:8000/articles" : "http://localhost:8000/articles?query=" + this.state.search_query
+    fetch(url).then(res => res.json()).then(json => {
+      console.log(json);
+      this.setState({ articles: json });
+    }).catch(error => console.log(error));
+  }
+
+  updateSearchQuery(new_query) {
+    this.setState({ articles: [] });
+    this.setState({ search_query: new_query }, () => this.getArticles());
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="flex-container">
+          <div className="app-header">
+            <b className="app-header-title">GLOBL</b>
+            <div>
+              <SearchBar onInput={val => this.updateSearchQuery(val)} />
+            </div>
+          </div>
+          <div className="article-container">
+            <ol className="article-list">
+              {this.state.articles.map(
+                article =>
+                  <Article key={article.title} title={article.title} description={article.description} url={article.url} publish_date={article.publish_date} />
+              )}
+            </ol>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Article.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Article extends React.Component {
+    render() {
+
+        let hoursDiff = (Date.now() - Date.parse(this.props.publish_date)) / 1000 / 3600;
+        let daysDiff = hoursDiff / 24;
+        return <li className="article-banner">
+            <div onClick={() => window.location = this.props.url}>
+                <h1>
+                    {this.props.title}
+                </h1>
+                <p>
+                    {this.props.description}
+                </p>
+                <p>
+                    Published {daysDiff >= 1 ? parseInt(daysDiff) + " days," : ""} {parseInt(hoursDiff % 24)} hours ago
+                </p>
+            </div>
+        </li>
+    }
+}
+
+export default Article;
